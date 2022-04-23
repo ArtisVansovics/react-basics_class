623274dc92aea11982396d6a0fefa861a4e1b83b
Artis Vansovics <vansovics.artis@gmail.com>
2022-04-23 15:29:55 +0300

    Constructed shape elements from an array using map and filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import Article from './components/Article/Article';
 import Hero from './components/Hero/Hero';
@@ -28,6 +27,75 @@ const articles = [
     onButtonClick: () => {
       console.log(56789);
     },
+  },
+];
+
+// Jāuztaisa masīvs, kurš sastāv no 10 objektiem, kuros ir:
+// krāsa
+// apaļš/kantains
+// rādīt/nerādīt
+// teksts ja teksts ir rādīt to pa vidu
+// ja nav tad rādīt "empty" un emty ir slīprakstā
+const shapesArr = [
+  {
+    color: 'red',
+    round: true,
+    visible: true,
+    text: '',
+  },
+  {
+    color: 'blue',
+    round: false,
+    visible: true,
+    text: 'Hello',
+  },
+  {
+    color: 'gold',
+    round: false,
+    visible: true,
+    text: 'Yes',
+  },
+  {
+    color: 'green',
+    round: true,
+    visible: false,
+    text: '',
+  },
+  {
+    color: 'pink',
+    round: true,
+    visible: true,
+    text: 'Maybe',
+  },
+  {
+    color: 'lightblue',
+    round: false,
+    visible: true,
+    text: '',
+  },
+  {
+    color: 'gray',
+    round: true,
+    visible: false,
+    text: 'So',
+  },
+  {
+    color: 'lightgreen',
+    round: true,
+    visible: true,
+    text: 'Im',
+  },
+  {
+    color: 'coral',
+    round: false,
+    visible: false,
+    text: '',
+  },
+  {
+    color: 'cyan',
+    round: true,
+    visible: true,
+    text: '',
   },
 ];
 
@@ -75,6 +143,29 @@ const App = () => (
                 onButtonClick={onButtonClick}
               />
             ))}
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12">
+          <div className="box">
+            {shapesArr
+              // filter out all visible ('true') shapes
+              .filter(({ visible }) => visible)
+              .map(({
+                color, round, text,
+              }) => (
+                <div
+                  // React requires each list child element to have a unique key
+                  key={Math.random()}
+                  // add .round class, if round is 'true'
+                  className={`shape ${round ? 'round' : ''}`}
+                  style={{ backgroundColor: color }}
+                >
+                  {/* if text is not empty, add text value, otherwise add 'empty' */}
+                  {text && text ? text : <em>Empty</em>}
+                </div>
+              ))}
           </div>
         </div>
       </div>
